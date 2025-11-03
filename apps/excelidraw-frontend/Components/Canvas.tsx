1cee9a59c8657a9130c14c6a72a0b65eddf13720
Sivakumar <nagireddysivakumar952@gmail.com>
2025-11-04 00:14:56 +0530

    add topbar

--- apps/excelidraw-frontend/Components/Canvas.tsx
+++ apps/excelidraw-frontend/Components/Canvas.tsx
@@ -1,42 +1,76 @@
+// import { initDraw } from "@/draw";
 "use client"
-import { initDraw } from "@/app/draw";
 import { useEffect, useRef, useState } from "react";
-import { Circle, Pencil, RectangleHorizontal } from "lucide-react";
 import { IconButton } from "./IconButton";
+import { Circle, Pencil, RectangleHorizontalIcon ,ArrowRight,EraserIcon} from "lucide-react";
+import { Game } from "@/app/draw/game";
 
-export type tool = "circle" | "rect" | "pencil";
+export type Tool = "circle" | "rect" | "line" |"eraser";
 
-export function Canvas({ roomId, socket }: {
-    socket: WebSocket
+export function Canvas({
+    roomId,
+    socket
+}: {
+    socket: WebSocket;
     roomId: string;
 }) {
     const canvasRef = useRef<HTMLCanvasElement>(null);
-    const [selectedTool, setSelectedTool] = useState<tool>("circle");
+    const [game, setGame] = useState<Game>();
+    const [selectedTool, setSelectedTool] = useState<Tool>("circle")
+
     useEffect(() => {
+        game?.setTool(selectedTool);
+    }, [selectedTool, game]);
+
+    useEffect(() => {
+
         if (canvasRef.current) {
-            initDraw(canvasRef.current, roomId, socket);
+            const g = new Game(canvasRef.current, roomId, socket);
+            setGame(g);
+
+            return () => {
+                g.destroy();
+            }
         }
-    }, [canvasRef])
-    return (
-        <div className="h-100vh overflow-hidden">
-            <Topbar setSelectedTool={setSelectedTool} selectedTool={selectedTool} />
-            <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight}></canvas>
-        </div>
-    )
+
+
+    }, [canvasRef]);
+
+    return <div style={{
+        height: "100vh",
+        overflow: "hidden"
+    }}>
+        <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight}></canvas>
+        <Topbar setSelectedTool={setSelectedTool} selectedTool={selectedTool} />
+    </div>
 }
 
-function Topbar({ selectedTool, setSelectedTool }: {
-    selectedTool: tool,
-    setSelectedTool: (s: tool) => void
+function Topbar({selectedTool, setSelectedTool}: {
+    selectedTool: Tool,
+    setSelectedTool: (s: Tool) => void
 }) {
-    return (
-        <div className="fixed top-10 left-10">
+    return <div style={{
+            position: "fixed",
+            top: 10,
+            left: 10
+        }}>
             <div className="flex gap-t">
-                <IconButton onClick={() => setSelectedTool("circle")} icon={<Circle />} activated={selectedTool == 'circle'} />
-                <IconButton onClick={() => setSelectedTool("rect")} icon={<RectangleHorizontal />} activated={selectedTool == 'rect'} />
-                <IconButton onClick={() => setSelectedTool("pencil")} icon={<Pencil />} activated={selectedTool == 'pencil'} />
+                <IconButton 
+                    onClick={() => {
+                        setSelectedTool("line")
+                    }}
+                    activated={selectedTool === "line"}
+                    icon={<ArrowRight />}
+                />
+                <IconButton onClick={() => {
+                    setSelectedTool("rect")
+                }} activated={selectedTool === "rect"} icon={<RectangleHorizontalIcon />} ></IconButton>
+                <IconButton onClick={() => {
+                    setSelectedTool("circle")
+                }} activated={selectedTool === "circle"} icon={<Circle />}></IconButton>
+                <IconButton onClick={() => {
+                    setSelectedTool("eraser")
+                }} activated={selectedTool === "eraser"} icon={<EraserIcon />}></IconButton>
             </div>
-
         </div>
-    )
 }
